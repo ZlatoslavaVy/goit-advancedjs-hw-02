40170bdce2f170752242c277a490e88da2571a78
Златослава Вишківська <zlatoslava6b@gmail.com>
2026-06-10 12:35:16 +0300

    add task 2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,6 +1,41 @@
-// Описаний у документації
 import iziToast from 'izitoast';
-// Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
 
-const refs = {};
+const refs = {
+  form: document.querySelector('.form'),
+};
+
+const onSubmit = event => {
+  event.preventDefault();
+
+  const delay = Number(event.currentTarget.elements.delay.value);
+  const state = event.currentTarget.elements.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay =>
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+        icon: '',
+      })
+    )
+    .catch(delay =>
+      iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+        icon: '',
+      })
+    );
+};
+
+refs.form.addEventListener('submit', onSubmit);
